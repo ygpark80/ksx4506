5d7c0873fbee6457f468deec4ac32f2bfeb91adc
YG Park <ygpark80@gmail.com>
2020-05-24 01:21:11 +0900

    command

--- src/commands.ts
+++ src/commands.ts
@@ -42,14 +42,26 @@ class Commands {
 			.command("breaker")
 			.action((args) => this.breaker(args))
 
+		vorpal
+			.command("command <deviceId> <subId> <commandType>")
+			.action((args) => this.command(args))
+
 		this.parser.on("data", (data) => {
 			const dataframe = KSX4506.parse(data)
 
+			if (dataframe.deviceId != 0x40 && dataframe.deviceId != 0x60) {
+				console.log(dataframe.toString())
+			}
+
 			// if (dataframe.deviceId != 0x40
-			// 	&& dataframe.deviceId != DeviceID.전등 && dataframe.deviceId != DeviceID.원격검침기 && dataframe.deviceId != DeviceID.대기전력차단기기
-			// 	// && dataframe.deviceId != DeviceID.일괄차단기 && dataframe.deviceId != DeviceID.가스밸브
+			// 	// && dataframe.deviceId != 0x60
+			// 	&& dataframe.deviceId != DeviceID.전등
+			// 	&& dataframe.deviceId != DeviceID.원격검침기
+			// 	&& dataframe.deviceId != DeviceID.대기전력차단기기
+			// 	&& dataframe.deviceId != DeviceID.일괄차단기
+			// 	&& dataframe.deviceId != DeviceID.가스밸브
 			// 	&& dataframe.deviceId != DeviceID.온도조절기
-			// 	&& dataframe.deviceId != 0x60) {
+			// 	) {
 			// 	this.vorpal.log("dataframe=", dataframe.toString())
 			// }
 
@@ -76,10 +88,14 @@ class Commands {
 			// 	this.vorpal.log("dataframe=", dataframe.toString())
 			// }
 
-			if (dataframe.deviceId == DeviceID.일괄차단기) {
-				this.vorpal.log("dataframe=", dataframe.toString())
-			}
+			// if (dataframe.deviceId == DeviceID.일괄차단기) {
+			// 	this.vorpal.log("dataframe=", dataframe.toString())
+			// }
 			// if (dataframe.deviceId == DeviceID.일괄차단기 && dataframe.commandType != CommandType.상태요구 && dataframe.commandType != CommandType.상태응답) {
+			// 	this.vorpal.log("dataframe=", dataframe.toString())
+			// }
+
+			// if (dataframe.deviceId == 0x40 && dataframe.commandType != CommandType.상태요구 && dataframe.commandType != CommandType.상태응답) {
 			// 	this.vorpal.log("dataframe=", dataframe.toString())
 			// }
 		})
@@ -89,11 +105,6 @@ class Commands {
 	async connect(args: Vorpal.Args) {
 		this.socket = net.connect({ host: "192.168.31.21", port: 8899 })
 		this.socket.pipe(this.parser)
-
-		// setInterval(() => {
-		// 	this.socket?.write(온도조절기.난방(우리집.온도조절기.침실1, "OFF").toBuffer())
-		// 	this.vorpal.log("wrote")
-		// }, 3000)
 	}
 
 	async disconnect(args: Vorpal.Args) {
@@ -119,6 +130,14 @@ class Commands {
 
 	async breaker(args: Vorpal.Args) {
 		const dataframe = new DataFrame(DeviceID.일괄차단기, 0x01, CommandType.특성요구)
+		this.socket?.write(dataframe.toBuffer())
+		this.vorpal.log(`${dataframe.toString()}`)
+	}
+
+	async command(args: Vorpal.Args) {
+		const { deviceId, subId, commandType } = args
+
+		const dataframe = new DataFrame(deviceId, subId, commandType)
 		this.socket?.write(dataframe.toBuffer())
 		this.vorpal.log(`${dataframe.toString()}`)
 	}
